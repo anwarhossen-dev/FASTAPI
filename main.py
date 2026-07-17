838a4e3a34a1023b32a4630f77ed559cf46fe30a
Md.Anwar hossen <anwarhossendeveloper21@gmail.com>
2026-07-17 23:24:31 +0600

    the second commit

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 
 
 app = FastAPI()
 
 #define requst body schema
+
 class Course(BaseModel):
     name: str
     instructor:str
     duration: float
     website: HttpUrl
 
-    @app.post("/post")
-    def create_post(Post:Course):
-        return{"data": Post}
+@app.post("/Post")
+def create_post(Post: Course):
+     return{"data": Post}
 
 @app.get("/")
 def devherozone():
